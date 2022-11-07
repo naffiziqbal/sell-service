b289394a872976a938688138e60d119897bb9ba9
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-07 23:28:34 +0600

    Connectiong To DB

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ app.use(cors())
 
 const port = process.env.PORT || 5000;
 // Connect To Database 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.zscbcon.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
