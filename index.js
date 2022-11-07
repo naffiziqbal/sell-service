fb3c96b7c5a96fca9a4b4fb108efa093379def6c
naffiziqbal <f.nafiziqbal@gmail.com>
2022-11-07 23:29:20 +0600

    Get Data From Db

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.zscbcon.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        app.get('/courses', async(req, res)=> {
+            const database = client.db('photographyCourse').collection('courseDetails');
+            const query = {};
+            const cursor = database.find(query)
+            const courses = await cursor.limit(3).toArray();
+            const count = await  database.estimatedDocumentCount() 
+            res.send({count,courses})
+        })
+ 
+
+    }
+    finally{
+        console.log("Best Of Luck, Keep Going");
+    }
+
+}
+run().catch(err=> console.log(err))
